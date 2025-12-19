7047d2e52436f677e20db7a4cc11a91350f48e80
Jati804 <jatiwesi22@gmail.com>
2025-12-19 17:20:38 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,9 +1,9 @@
 export const CONTACT_INFO = {
   // Ganti nomor di sini (Format Teks)
-  phoneDisplay: "+62 856-9561-4669",
+  phoneDisplay: "+62 813-1548-8000",
   
   // Ganti nomor di sini (Format Link WhatsApp: Awali dengan 62 tanpa + atau spasi)
-  waNumber: "6285695614669",
+  waNumber: "6281315488000",
   
   // Pesan default saat orang klik WA
   defaultMessage: "Halo SANUR Akademi Inspirasi, saya tertarik untuk konsultasi program."
