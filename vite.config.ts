6f8f8ca579c9d23f93c2e582a9e16e58b68ef6bd
Jati804 <jatiwesi22@gmail.com>
2025-12-20 10:38:30 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  // UBAH: Dari 'base: /' menjadi 'base: ""' (kosong/relatif)
+  base: "", 
   plugins: [react()],
   build: {
     outDir: 'dist',
