999fe3216f39ea7b5e5b01b0849bf2c9d34f4135
Jati804 <jatiwesi22@gmail.com>
2025-12-20 11:54:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // WAJIB DITAMBAHKAN: BASE PATH UNTUK VERSEL/BROWSER ROUTER
+  // WAJIB DITAMBAHKAN: BASE PATH ABSOLUT untuk Vercel/BrowserRouter
   base: '/',
   plugins: [react()],
   publicDir: 'public',
